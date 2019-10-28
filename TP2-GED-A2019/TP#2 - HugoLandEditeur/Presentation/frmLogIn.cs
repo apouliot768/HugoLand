3c568f12aa839b7c358fbaad86f0d2282c2277ad
Alexandre Pouliot <apouliot768@gmail.com>
2019-10-28 11:17:04 -0400

    Update frmLogIn.cs

--- TP2-GED-A2019/TP#2 - HugoLandEditeur/Presentation/frmLogIn.cs
+++ TP2-GED-A2019/TP#2 - HugoLandEditeur/Presentation/frmLogIn.cs
@@ -40,25 +40,8 @@ namespace HugoLandEditeur.Presentation
 
         private void loadFont()
         {
-            //Create your private font collection object.
             PrivateFontCollection pfc = new PrivateFontCollection();
-
-            //Select your font from the resources.
-            //My font here is "Digireu.ttf"
-            int fontLength = Properties.Resources.DiabloHeavy.Length;
-
-            // create a buffer to read in to
-            byte[] fontdata = Properties.Resources.DiabloHeavy;
-
-            // create an unsafe memory block for the font data
-            System.IntPtr data = Marshal.AllocCoTaskMem(fontLength);
-
-            // copy the bytes to the unsafe memory block
-            Marshal.Copy(fontdata, 0, data, fontLength);
-
-            // pass the font to the font collection
-            pfc.AddMemoryFont(data, fontLength);
-
+            pfc.AddFontFile(@"..\..\Resources\DiabloHeavy.ttf");
             lblTitre1.Font = new Font(pfc.Families[0], lblTitre1.Font.Size);
             label1.Font = new Font(pfc.Families[0], label1.Font.Size, FontStyle.Underline);
         }
